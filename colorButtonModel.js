ef3cc1dc0394a9b297e3ab90ea25d16d09e09fa2
fhj <307890+freddyh@users.noreply.github.com>
2020-04-30 00:05:18 -0700

    added model for buttons

---
+++ colorButtonModel.js
@@ -0,0 +1,6 @@
+export default class ColorButtonModel {
+    constructor(props) {
+        this.color = props.color;
+        this.mutable = props.mutable;
+    }
+}
--- components/Board.tsx
+++ components/Board.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import ColorButton from './ColorButton';
 import ColorManager from '../colorManager';
+import ColorButtonModel from '../colorButtonModel';
 
 const style = StyleSheet.create({
   container: {
@@ -34,17 +35,19 @@ export default class Board extends Component {
 
         while (i < count) {
             const random = this.game.randomRow(4);
-            rows.push(random);
+            const models = random.map((color) => { return new ColorButtonModel({color: color, mutable: true }); });
+            rows.push(models);
             i++;
         }
-        rows.push(colors);
+        const immutableColors = colors.map((color) => { return new ColorButtonModel({color: color, mutable: false }); });
+        rows.push(immutableColors);
 
         return (
             <View style={style.container}>
                 {rows.map((row, index) => {
                     return <View key={index} style={style.row}>
-                        {row.map((color, index) => {
-                            return <ColorButton key={`${color}-${index}`} colorName={color} mutable={true}/>
+                        {row.map((model, index) => {
+                            return (<ColorButton key={`${model}-${index}`} colorName={model.color} mutable={model.mutable}/>);
                         })}
                     </View>
                 })}
