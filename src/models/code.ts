0f3e9997081e74aa5a2a563909c263a465699203
fhj <307890+freddyh@users.noreply.github.com>
2020-05-13 22:52:50 -0700

    updated code comparison algorithm

--- src/models/code.ts
+++ src/models/code.ts
@@ -1,9 +1,13 @@
 export class CodeComparisonResult {
-  static readonly FULL_MATCH  = new CodeComparisonResult('FULL_MATCH', 'black');
+  static readonly FULL_MATCH = new CodeComparisonResult('FULL_MATCH', 'black');
   static readonly PARTIAL_MATCH = new CodeComparisonResult('PARTIAL_MATCH', 'white');
 
   // private to disallow creating other instances of this type
   private constructor(private readonly key: string, public readonly value: string) {
+  }
+
+  debugDescription() {
+    return `key: ${this.key} value: ${this.value}`;
   }
 
   toString() {
@@ -32,6 +36,14 @@ export default class Code {
     let burned: number[] = [];
 
     for (let i = 0; i < a.length; i++) {
+      // console.log(`i: ${i}`);
+      if (a[i] === b[i]) {
+        results.push(CodeComparisonResult.FULL_MATCH);
+        burned.push(i);
+        // console.log(`full match, burn ${i}, continue`);
+        continue;
+      }
+
       for (let j = 0; j < b.length; j++) {
         // console.log(`i: ${i}, j: ${j}`);
 
@@ -41,20 +53,20 @@ export default class Code {
             results.push(CodeComparisonResult.FULL_MATCH);
             burned.push(j);
             // console.log(`full match, burn ${j}, break`);
-            break;  
+            break;
           }
 
           if (burned.includes(j)) {
             // console.log(`${j} is burned, continue`);
             continue;
           }
-  
+
           const willBeColorMatch = a[j] === b[j];
           if (willBeColorMatch) {
             // console.log(`predict full match, burn ${j}, break`);
             continue;
           }
-  
+
           results.push(CodeComparisonResult.PARTIAL_MATCH);
           burned.push(j);
           // console.log(`got a match, burn ${j}, break`);
